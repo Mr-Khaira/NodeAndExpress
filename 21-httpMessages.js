5c9bd20ac58fb32639cd2b2e5ef8e3f25a5e70d0
Mr Khaira <singhkhairaharkaran@gmail.com>
2024-02-03 17:31:23 -0500

    Now in other repo

---
+++ 21-httpMessages.js
@@ -0,0 +1,18 @@
+/* 
+
+Request Messages is what the user is sending(basically the web application).
+
+Response message is what the the server responds with.
+
+HTTP Methods :-
+
+GET - read data
+POST - insert data
+PUT - update data
+DELETE - delete data
+
+Headers -  are key value pairs, just some meta data about our request.
+
+Body - When the want to post a resource, we are expected to have a body.
+Body is also called payload.
+*/
